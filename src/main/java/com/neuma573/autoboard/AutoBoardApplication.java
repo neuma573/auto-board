bb355d74bdb9cc1d0802c0f129061c6606d10ed6
neuma573 <crye@kakao.com>
2023-11-20 23:05:52 +0900

    initial commit

---
+++ src/main/java/com/neuma573/autoboard/AutoBoardApplication.java
@@ -0,0 +1,13 @@
+package com.neuma573.autoboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neuma573/autoboard/AutoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuma573.autoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
