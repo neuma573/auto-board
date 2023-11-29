48d103506e5929d084da7f8bd2614e37a0a6353d
neuma573 <crye@kakao.com>
2023-11-30 02:12:23 +0900

    feat: 사용자 인증을 위해 LoginUser 클래스로 UserDetails 구현

---
+++ src/main/java/com/neuma573/autoboard/security/model/dto/LoginUser.java
@@ -0,0 +1,58 @@
+package com.neuma573.autoboard.security.model.dto;
+
+import com.neuma573.autoboard.user.model.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@RequiredArgsConstructor
+public class LoginUser implements UserDetails {
+
+    private final User user;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+}
