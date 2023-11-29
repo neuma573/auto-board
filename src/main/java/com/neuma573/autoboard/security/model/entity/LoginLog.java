4ffc56dfc9baa6c37bc0fedc3f5f44f74ac1c99d
neuma573 <crye@kakao.com>
2023-11-30 02:08:24 +0900

    feat: 로그인 로그를 등록하는 login_log 엔티티 작성

---
+++ src/main/java/com/neuma573/autoboard/security/model/entity/LoginLog.java
@@ -0,0 +1,36 @@
+package com.neuma573.autoboard.security.model.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "login_log")
+public class LoginLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "login_time")
+    private LocalDateTime loginTime;
+
+    @Column(name = "ip_address")
+    private String ipAddress;
+
+    @Column(name = "login_result")
+    private String loginResult;
+
+    @Column(name = "error_message")
+    private String errorMessage;
+
+    @Column(name = "device_info")
+    private String deviceInfo;
+
+    @Column(name = "session_id")
+    private String sessionId;
+
+}
