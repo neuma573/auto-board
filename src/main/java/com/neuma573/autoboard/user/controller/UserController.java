3eb229ffb04c69d11a34c2a2eed41abd535148a8
“neuma573” <crye@kakao.com>
2023-12-03 23:53:07 +0900

    feat: 회원가입 엔드포인트 추가

--- src/main/java/com/neuma573/autoboard/user/controller/UserController.java
+++ src/main/java/com/neuma573/autoboard/user/controller/UserController.java
@@ -3,20 +3,30 @@ package com.neuma573.autoboard.user.controller;
 
 import com.neuma573.autoboard.user.model.dto.UserRequest;
 import com.neuma573.autoboard.user.model.dto.UserResponse;
+import com.neuma573.autoboard.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/v1")
+@RequiredArgsConstructor
 @RestController
 public class UserController {
 
-    @PostMapping(value = "/users")
-    public ResponseEntity<UserResponse> register() {
+    private final UserService userService;
+
+
+    @GetMapping(value = "/test")
+    public ResponseEntity<String> test() {
+
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/users")
+    public ResponseEntity<UserResponse> join(@Valid @RequestBody UserRequest userRequest) {
+
+        return new ResponseEntity<>(userService.join(userRequest),HttpStatus.OK);
     }
 }
