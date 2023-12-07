77cc7f95ca557c6ebf2468261d2147be38021b0a
neuma573 <crye@kakao.com>
2023-12-08 08:43:41 +0900

    feat: oauth 인증 및 일반 로그인을 위한 컨트롤러 추가

---
+++ src/main/java/com/neuma573/autoboard/security/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.neuma573.autoboard.security.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+public class AuthController {
+}
