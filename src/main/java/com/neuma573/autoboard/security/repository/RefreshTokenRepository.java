c691241c43f484eb18d6451ba742f664cdb57bbe
neuma573 <crye@kakao.com>
2023-12-09 01:05:25 +0900

    feat: 토큰으로 select하는 findByToken 메서드 작성

--- src/main/java/com/neuma573/autoboard/security/repository/RefreshTokenRepository.java
+++ src/main/java/com/neuma573/autoboard/security/repository/RefreshTokenRepository.java
@@ -3,6 +3,10 @@ package com.neuma573.autoboard.security.repository;
 import com.neuma573.autoboard.security.model.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String token);
 
 }
