e11be87c974d681efdc39fd8dc210bcd0a0fa3fc
neuma573 <crye@kakao.com>
2024-01-11 04:21:08 +0900

    feat : 로그인 및 refreshToken 재발급 api 작성

--- src/main/java/com/neuma573/autoboard/security/controller/AuthController.java
+++ src/main/java/com/neuma573/autoboard/security/controller/AuthController.java
@@ -1,21 +1,24 @@
 package com.neuma573.autoboard.security.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.neuma573.autoboard.global.model.dto.Response;
+import com.neuma573.autoboard.global.utils.ResponseUtils;
 import com.neuma573.autoboard.security.model.dto.Jwt;
-import com.neuma573.autoboard.security.model.dto.TokenRequest;
+import com.neuma573.autoboard.security.service.AuthService;
 import com.neuma573.autoboard.security.service.TokenService;
 import com.neuma573.autoboard.user.model.dto.LoginRequest;
-import com.neuma573.autoboard.user.model.dto.LoginResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/v1/auth")
+import java.io.IOException;
+
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Slf4j
 @RestController
@@ -23,19 +26,23 @@ public class AuthController {
 
     private final TokenService tokenService;
 
+    private final AuthService authService;
+
+    private final ResponseUtils responseUtils;
+
     @PostMapping("/refresh/token")
-    public ResponseEntity<Jwt> tokenRefresh(@RequestBody TokenRequest tokenRequest) throws JsonProcessingException {
-        Jwt jwt = tokenService.refreshToken(tokenRequest.getRefreshToken());
-        if (jwt == null){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(null);
-        }
-        return ResponseEntity.ok(jwt);
+    public ResponseEntity<Response<Jwt>> tokenRefresh(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+
+        return ResponseEntity.ok(responseUtils.success(
+                tokenService.refreshAccessToken(httpServletRequest, httpServletResponse)
+        ));
     }
 
-    @PostMapping("/signin")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-        return null;
-    }
+    @PostMapping("/authenticate")
+    public ResponseEntity<Response<?>> login(@RequestBody LoginRequest loginRequest, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
 
+        return ResponseEntity.ok(responseUtils.success(
+                authService.verifyUser(loginRequest, httpServletRequest, httpServletResponse)
+        ));
+    }
 }
