cd3f96dd798eff7b489461b381caa9254a1b8d40
neuma573 <crye@kakao.com>
2024-01-11 05:01:56 +0900

    feat: API 응답값 표준화를 위해 HTTP 응답 설정, 성공 및 오류 응답 생성을 위한 메서드를 제공하는 ResponseUtils 작성

---
+++ src/main/java/com/neuma573/autoboard/global/utils/ResponseUtils.java
@@ -0,0 +1,58 @@
+package com.neuma573.autoboard.global.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.neuma573.autoboard.global.exception.ExceptionCode;
+import com.neuma573.autoboard.global.model.dto.Response;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ResponseUtils {
+
+    private final ObjectMapper objectMapper;
+
+    public void setResponse(HttpServletResponse response, ExceptionCode exceptionCode, Exception e) throws IOException {
+        log.error("error message : {}", exceptionCode.getMessage(), e);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        response.getWriter().write(objectMapper.writeValueAsString(
+                Response.builder()
+                .status(exceptionCode.getStatus().value())
+                .message(exceptionCode.getMessage())
+                .build()
+        ));
+    }
+
+    public <T> Response<T> success(T data) {
+        return Response.<T>builder()
+                .status(HttpStatus.OK.value())
+                .data(data)
+                .build();
+    }
+
+    public <T> Response<T> created(T data) {
+        return Response.<T>builder()
+                .status(HttpStatus.CREATED.value())
+                .data(data)
+                .build();
+    }
+
+    public Response<String> error(ExceptionCode status, Exception e) {
+        return Response.<String>builder()
+                .status(status.getStatus().value())
+                .message(status.getMessage())
+                .data(e.getClass().getSimpleName())
+                .build();
+    }
+
+}
