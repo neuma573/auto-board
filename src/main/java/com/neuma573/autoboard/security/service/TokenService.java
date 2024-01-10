52996cea0e6676b1e528608540ab60d6f64daf87
neuma573 <crye@kakao.com>
2024-01-11 05:04:47 +0900

    fix: jwt 관련 로직은 Provider로 이동

--- src/main/java/com/neuma573/autoboard/security/service/TokenService.java
+++ src/main/java/com/neuma573/autoboard/security/service/TokenService.java
@@ -1,58 +1,24 @@
 package com.neuma573.autoboard.security.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.neuma573.autoboard.security.filter.VerifyUserFilter;
-import com.neuma573.autoboard.security.model.dto.AuthenticateUser;
-import com.neuma573.autoboard.security.model.dto.Jwt;
-import com.neuma573.autoboard.security.model.entity.RefreshToken;
-import com.neuma573.autoboard.security.repository.RefreshTokenRepository;
+import com.neuma573.autoboard.security.model.dto.AccessTokenResponse;
 import com.neuma573.autoboard.security.utils.JwtProvider;
-import com.neuma573.autoboard.user.model.entity.User;
-import com.neuma573.autoboard.user.model.entity.UserRole;
-import com.neuma573.autoboard.user.repository.UserRepository;
-import io.jsonwebtoken.Claims;
-import jakarta.persistence.EntityNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.stream.Collectors;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Service
 public class TokenService {
 
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final UserRepository userRepository;
-
     private final JwtProvider jwtProvider;
 
-    private final ObjectMapper objectMapper;
-
-    public void storeRefreshToken(String loginId, String token) {
-        RefreshToken refreshToken = RefreshToken
-                .builder()
-                .loginId(loginId)
-                .token(token)
-                .build();
-        refreshTokenRepository.save(refreshToken);
-    }
-
     @Transactional
-    public Jwt refreshToken(String refreshToken) throws JsonProcessingException {
-        Claims claims = jwtProvider.getClaims(refreshToken);
-
-        RefreshToken token = refreshTokenRepository.findByToken(refreshToken).orElseThrow(() -> new EntityNotFoundException("Token not found"));
-
-        User user = userRepository.findByLoginId(token.getLoginId()).orElseThrow(() -> new EntityNotFoundException("User not found"));
-
-        AuthenticateUser authenticateUser = new AuthenticateUser(user.getLoginId(),
-                user.getRoles().stream().map(UserRole::getRole).collect(Collectors.toSet()));
-        String authenticateUserJson = objectMapper.writeValueAsString(authenticateUser);
-        claims.put(VerifyUserFilter.AUTHENTICATE_USER,authenticateUserJson);
-        Jwt jwt = jwtProvider.createJwt(claims);
-        token.setToken(jwt.getRefreshToken());
-        return jwt;
+    public AccessTokenResponse refreshAccessToken(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        return jwtProvider.refreshAccessToken(httpServletRequest, httpServletResponse);
     }
 }
