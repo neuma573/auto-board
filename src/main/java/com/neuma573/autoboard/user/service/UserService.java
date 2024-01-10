52b4a0f14d81778e0c73af9356534258dcff8b0f
neuma573 <crye@kakao.com>
2024-01-11 05:06:50 +0900

    fix: 로그인 검증은 AuthService로 이동

--- src/main/java/com/neuma573/autoboard/user/service/UserService.java
+++ src/main/java/com/neuma573/autoboard/user/service/UserService.java
@@ -1,27 +1,24 @@
 package com.neuma573.autoboard.user.service;
 
-import com.neuma573.autoboard.security.model.dto.VerifyResponse;
 import com.neuma573.autoboard.security.utils.PasswordEncoder;
-import com.neuma573.autoboard.user.model.dto.LoginRequest;
 import com.neuma573.autoboard.user.model.dto.UserRequest;
 import com.neuma573.autoboard.user.model.dto.UserResponse;
 import com.neuma573.autoboard.user.model.entity.User;
 import com.neuma573.autoboard.user.model.entity.UserRole;
 import com.neuma573.autoboard.user.model.enums.Role;
 import com.neuma573.autoboard.user.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
     private final UserRoleRepository userRoleRepository;
+
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
@@ -33,36 +30,16 @@ public class UserService {
                 )
         );
 
-        User user = userRepository.save(
-                userRequest.toEntity()
-        );
-
+        User user = userRequest.toEntity();
         UserRole role = UserRole.builder()
                 .role(Role.USER)
                 .user(user)
                 .build();
         user.addRole(role);
+        userRepository.save(user);
         userRoleRepository.save(role);
         return user.toResponse();
 
-    }
-
-    @Transactional
-    public VerifyResponse verifyUser(LoginRequest loginRequest){
-        User user = userRepository.findByLoginId(loginRequest.getLoginId()).orElseThrow(() -> new EntityNotFoundException("User not found"));
-        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-            user.addFailCount();
-            return VerifyResponse
-                    .builder()
-                    .isValid(false)
-                    .build();
-        }
-
-        return VerifyResponse
-                .builder()
-                .isValid(true)
-                .userRole(user.getRoles().stream().map(UserRole::getRole).collect(Collectors.toSet()))
-                .build();
     }
 
 }
