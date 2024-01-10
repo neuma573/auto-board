d5d553fb5a431652c66160d1bc60482f1efbd13f
neuma573 <crye@kakao.com>
2024-01-11 04:57:37 +0900

    fix: 정적메서드는 util 클래스로 따로 작성하였음. 생성자 대신 좀더 익숙한 빌더패턴을 사용

--- src/main/java/com/neuma573/autoboard/global/model/dto/Response.java
+++ src/main/java/com/neuma573/autoboard/global/model/dto/Response.java
@@ -1,23 +1,17 @@
 package com.neuma573.autoboard.global.model.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Response<T> {
 
-    private String status;
+    private int status;
     private String message;
     private T data;
-
-    public static <T> Response<T> success(String message, T data){
-        return new Response<>("Success", message, data);
-    }
-
-    public static Response<Void> error(String status, String message){
-        return new Response<>(status, message, null);
-    }
 }
