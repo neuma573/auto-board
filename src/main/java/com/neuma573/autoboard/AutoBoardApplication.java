4306e143eba207640f55ed7fab5f08108e4b0b0d
neuma573 <crye@kakao.com>
2024-01-11 04:26:50 +0900

    feat: Spring Data Redis 리포지토리 활성화

--- src/main/java/com/neuma573/autoboard/AutoBoardApplication.java
+++ src/main/java/com/neuma573/autoboard/AutoBoardApplication.java
@@ -3,8 +3,10 @@ package com.neuma573.autoboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @EnableJpaAuditing
+@EnableRedisRepositories
 @SpringBootApplication
 public class AutoBoardApplication {
 
