f2730866cfb2878daf5fe3df01b5609ea472f440
neuma573 <crye@kakao.com>
2024-01-11 05:27:30 +0900

    feat: 메인화면, 로그인을 위한 정적 요소 추가

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        let username = document.getElementById('username').value;
+        let password = document.getElementById('password').value;
+
+        fetch('/api/v1/auth/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ loginId: username, password: password }),
+        })
+            .then(response => {
+                // status 코드에 따라 다른 처리를 합니다.
+                if (response.status === 200) {
+                    return response.json(); // 성공 응답 처리
+                } else {
+                    throw response; // 에러 응답 처리
+                }
+            })
+            .then(data => {
+                console.log('Success:', data);
+                alert('로그인 성공:');
+                localStorage.setItem("accessToken", data.data.accessToken);
+                window.location.href = "/";
+            })
+            .catch((error) => {
+                error.json().then(errMessage => {
+                    console.error('Error:', errMessage);
+                    alert(errMessage.message); // 에러 메시지 표시
+                });
+            });
+    });
+});
