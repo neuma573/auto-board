5a784e4a3da7db32189ac396d2f79340a4f01869
neuma573 <crye@kakao.com>
2024-01-11 05:03:34 +0900

    fix: 로그인 엔드포인트는 AuthController로 이동, 커스텀 응답 반환하도록 적용

--- src/main/java/com/neuma573/autoboard/user/controller/UserController.java
+++ src/main/java/com/neuma573/autoboard/user/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.neuma573.autoboard.user.controller;
 
 
-import com.neuma573.autoboard.user.model.dto.LoginRequest;
-import com.neuma573.autoboard.user.model.dto.LoginResponse;
+import com.neuma573.autoboard.global.model.dto.Response;
+import com.neuma573.autoboard.global.utils.ResponseUtils;
 import com.neuma573.autoboard.user.model.dto.UserRequest;
 import com.neuma573.autoboard.user.model.dto.UserResponse;
 import com.neuma573.autoboard.user.service.UserService;
@@ -13,13 +13,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("api/v1")
+import java.net.URI;
+
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Slf4j
 @RestController
 public class UserController {
 
     private final UserService userService;
+
+    private final ResponseUtils responseUtils;
 
 
     @GetMapping(value = "/test")
@@ -29,12 +33,8 @@ public class UserController {
     }
 
     @PostMapping(value = "/users")
-    public ResponseEntity<UserResponse> join(@Valid @RequestBody UserRequest userRequest) {
-        return new ResponseEntity<>(userService.signUp(userRequest), HttpStatus.CREATED);
+    public ResponseEntity<Response<UserResponse>> join(@Valid @RequestBody UserRequest userRequest) {
+        return ResponseEntity.created(URI.create("/main")).body(responseUtils.created(userService.signUp(userRequest)));
     }
 
-//    @PostMapping(value = "/login")
-//    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
-//        return new ResponseEntity<>(userService.login(loginRequest), HttpStatus.OK);
-//    }
 }
