023a26874c9fa9afa8a62c79201ebe16bb40bfbc
neuma573 <crye@kakao.com>
2024-01-11 05:18:47 +0900

    feat: 쿠키 생성, 조회, 삭제를 위한 유틸리티 추가

---
+++ src/main/java/com/neuma573/autoboard/security/utils/CookieUtils.java
@@ -0,0 +1,40 @@
+package com.neuma573.autoboard.security.utils;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieUtils {
+
+    public static Cookie createCookie(String name, String value, int maxAge, boolean isSecure, boolean isHttpOnly) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setMaxAge(maxAge);
+        cookie.setPath("/");
+        cookie.setSecure(isSecure);
+        cookie.setHttpOnly(isHttpOnly);
+        return cookie;
+    }
+
+    public static void addCookie(HttpServletResponse response, Cookie cookie) {
+        response.addCookie(cookie);
+    }
+
+    public static String getCookieValue(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(cookieName)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void deleteCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+}
