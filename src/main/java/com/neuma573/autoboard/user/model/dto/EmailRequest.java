cafb2607687d6ef88afe76e0641e526b0909dbd8
neuma573 <crye@kakao.com>
2024-01-12 06:08:55 +0900

    feat: 이메일 중복 검증을 위한 DTO 작성

---
+++ src/main/java/com/neuma573/autoboard/user/model/dto/EmailRequest.java
@@ -0,0 +1,16 @@
+package com.neuma573.autoboard.user.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailRequest {
+
+    @NotBlank(message = "이메일은 비어 있을 수 없습니다.")
+    @Email(message = "유효하지 않은 이메일 형식입니다.")
+    private String email;
+
+}
