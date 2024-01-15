234a9cc1148f42b0a2fd72cc6b04934eb7db229b
neuma573 <crye@kakao.com>
2024-01-16 06:48:14 +0900

    feat: 회원가입 dto에서 패스워드 bean validation 추가

--- src/main/java/com/neuma573/autoboard/user/model/dto/UserRequest.java
+++ src/main/java/com/neuma573/autoboard/user/model/dto/UserRequest.java
@@ -4,6 +4,8 @@ import com.neuma573.autoboard.global.model.enums.Status;
 import com.neuma573.autoboard.user.model.entity.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,6 +25,8 @@ public class UserRequest {
     @NotEmpty(message = "이름은 비어있을 수 없습니다.")
     private String name;
 
+    @Size(min = 8, message = "패스워드는 최소 8자 이상이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]{8,}$", message = "패스워드는 숫자, 문자, 특수문자를 포함해야 합니다.")
     @NotEmpty(message = "패스워드는 비어있을 수 없습니다.")
     @Setter
     private String password;
