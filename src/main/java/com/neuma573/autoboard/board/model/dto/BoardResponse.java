c1122897fcd06b30719b5defd9cc0747429aab26
neuma573 <crye@kakao.com>
2024-01-18 03:13:35 +0900

    feat: entity를 dto로 변환할 정적메서드 작성

--- src/main/java/com/neuma573/autoboard/board/model/dto/BoardResponse.java
+++ src/main/java/com/neuma573/autoboard/board/model/dto/BoardResponse.java
@@ -1,7 +1,10 @@
 package com.neuma573.autoboard.board.model.dto;
 
+import com.neuma573.autoboard.board.model.entity.Board;
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 public class BoardResponse {
 
@@ -11,4 +14,12 @@ public class BoardResponse {
 
     private Long postCount;
 
+
+    public static BoardResponse of(Board board) {
+        return BoardResponse.builder()
+                .id(board.getId())
+                .boardName(board.getName())
+                .postCount((long) board.getPosts().size())
+                .build();
+    }
 }
