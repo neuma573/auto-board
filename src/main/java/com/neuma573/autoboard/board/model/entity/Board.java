ff97e1da24dad4b496aec12f07e092344537f8e8
neuma573 <crye@kakao.com>
2024-01-18 03:13:10 +0900

    feat: 게시판에 삭제여부 필드 추가, 게시판 사용자를 담을  ConcurrentHashMap users 추가

--- src/main/java/com/neuma573/autoboard/board/model/entity/Board.java
+++ src/main/java/com/neuma573/autoboard/board/model/entity/Board.java
@@ -4,9 +4,12 @@ import com.neuma573.autoboard.global.model.entity.BaseEntity;
 import com.neuma573.autoboard.post.model.entity.Post;
 import com.neuma573.autoboard.user.model.entity.User;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
+@Getter
 @Entity
 public class Board extends BaseEntity {
 
@@ -23,5 +26,15 @@ public class Board extends BaseEntity {
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Post> posts;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "board_users",
+            joinColumns = @JoinColumn(name = "board_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users = ConcurrentHashMap.newKeySet();
+
     private boolean isPublic;
+
+    private boolean isDeleted;
 }
