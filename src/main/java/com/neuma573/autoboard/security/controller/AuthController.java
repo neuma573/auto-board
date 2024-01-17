5d823e56eb6508702e29a26e0a25e25ad12ad538
neuma573 <crye@kakao.com>
2024-01-18 04:08:53 +0900

    refactor: restful한 메서드 선택(수정 -> PUT Method)

--- src/main/java/com/neuma573/autoboard/security/controller/AuthController.java
+++ src/main/java/com/neuma573/autoboard/security/controller/AuthController.java
@@ -11,10 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -30,7 +27,7 @@ public class AuthController {
 
     private final ResponseUtils responseUtils;
 
-    @PostMapping("/refresh/token")
+    @PutMapping("/refresh/token")
     public ResponseEntity<Response<AccessTokenResponse>> tokenRefresh(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
         return ResponseEntity.ok(responseUtils.success(
                 tokenService.refreshAccessToken(httpServletRequest, httpServletResponse)
