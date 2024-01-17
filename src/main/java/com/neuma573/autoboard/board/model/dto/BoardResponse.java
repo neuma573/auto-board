cc262c45782906e975cf2959cca9fc7e6650ad3a
neuma573 <crye@kakao.com>
2024-01-17 20:29:24 +0900

    feat: 게시판 조회 api를 위한 dto 작성

---
+++ src/main/java/com/neuma573/autoboard/board/model/dto/BoardResponse.java
@@ -0,0 +1,14 @@
+package com.neuma573.autoboard.board.model.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BoardResponse {
+
+    private Long id;
+
+    private String boardName;
+
+    private Long postCount;
+
+}
