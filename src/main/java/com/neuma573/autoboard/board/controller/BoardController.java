88ad0c00aff2a14b896b5fa7fbfb627dda9d0d03
neuma573 <crye@kakao.com>
2024-01-18 09:44:15 +0900

    feat: 게시판 리스트 출력 / 등록 api 작성

--- src/main/java/com/neuma573/autoboard/board/controller/BoardController.java
+++ src/main/java/com/neuma573/autoboard/board/controller/BoardController.java
@@ -1,7 +1,43 @@
 package com.neuma573.autoboard.board.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.neuma573.autoboard.board.model.dto.BoardRequest;
+import com.neuma573.autoboard.board.model.dto.BoardResponse;
+import com.neuma573.autoboard.board.service.BoardService;
+import com.neuma573.autoboard.global.model.dto.Response;
+import com.neuma573.autoboard.global.utils.ResponseUtils;
+import com.neuma573.autoboard.security.utils.JwtProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.util.List;
+
+
+@Slf4j
+@RequestMapping("/api/v1/board")
+@RequiredArgsConstructor
 @RestController
 public class BoardController {
+
+    private final BoardService boardService;
+
+    private final JwtProvider jwtProvider;
+
+    private final ResponseUtils responseUtils;
+
+    @GetMapping("")
+    public ResponseEntity<Response<List<BoardResponse>>> getBoardList(HttpServletRequest httpServletRequest) {
+        String email = jwtProvider.parseEmailFrom(httpServletRequest);
+        return ResponseEntity.ok().body(responseUtils.success(boardService.getBoardList(email)));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Response<BoardResponse>> saveBoard(@Valid @RequestBody BoardRequest boardRequest, HttpServletRequest httpServletRequest) {
+        String email = jwtProvider.parseEmailWithValidation(httpServletRequest);
+        return ResponseEntity.created(URI.create("/main")).body(responseUtils.created(boardService.saveBoard(email, boardRequest)));
+    }
 }
