ca21e9459f5c45fe391237b27578a3087c02f1c7
neuma573 <crye@kakao.com>
2024-01-18 09:39:37 +0900

    feat: 게시판 저장 로직 구현

--- src/main/java/com/neuma573/autoboard/board/service/BoardService.java
+++ src/main/java/com/neuma573/autoboard/board/service/BoardService.java
@@ -1,8 +1,10 @@
 package com.neuma573.autoboard.board.service;
 
+import com.neuma573.autoboard.board.model.dto.BoardRequest;
 import com.neuma573.autoboard.board.model.dto.BoardResponse;
 import com.neuma573.autoboard.board.repository.BoardRepository;
 import com.neuma573.autoboard.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +19,17 @@ public class BoardService {
 
     private final UserRepository userRepository;
 
+    @Transactional
     public List<BoardResponse> getBoardList(String email) {
         return boardRepository.findPublicAndNotDeletedBoardWith(userRepository.findByEmail(email))
                 .stream()
                 .map(BoardResponse::of).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public BoardResponse saveBoard(String email, BoardRequest boardRequest) {
+        return BoardResponse.of(boardRepository.save(boardRequest.toEntity(
+                userRepository.findByEmail(email).orElseThrow()
+        )));
     }
 }
