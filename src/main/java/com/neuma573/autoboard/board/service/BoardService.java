b26759e2c66c4930fcf1efab894d51ed88bf8453
neuma573 <crye@kakao.com>
2024-01-21 05:23:03 +0900

    feat : Board Entity에 유저가 접근가능한지를 반환하는 메서드 서비스 구현, Board DTO 변환 메서드 작성

--- src/main/java/com/neuma573/autoboard/board/service/BoardService.java
+++ src/main/java/com/neuma573/autoboard/board/service/BoardService.java
@@ -2,15 +2,21 @@ package com.neuma573.autoboard.board.service;
 
 import com.neuma573.autoboard.board.model.dto.BoardRequest;
 import com.neuma573.autoboard.board.model.dto.BoardResponse;
+import com.neuma573.autoboard.board.model.entity.Board;
 import com.neuma573.autoboard.board.repository.BoardRepository;
+import com.neuma573.autoboard.global.exception.BoardNotFoundException;
+import com.neuma573.autoboard.global.exception.UserNotFoundException;
+import com.neuma573.autoboard.user.model.entity.User;
 import com.neuma573.autoboard.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class BoardService {
@@ -32,4 +38,19 @@ public class BoardService {
                 userRepository.findByEmail(email).orElseThrow()
         )));
     }
+
+    @Transactional()
+    public boolean checkAccessible(Long boardId, String email) {
+        Board board  = boardRepository.findById(boardId).orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시판입니다."));
+
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("존재하지 않는 유저입니다."));
+
+        return board.isAccessible(user);
+    }
+
+    @Transactional
+    public BoardResponse getBoardInfo(Long boardId) {
+        return BoardResponse.of(boardRepository.findById(boardId).orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시판입니다.")));
+    }
+
 }
