ffde0ddb847fba6cd345411619036a30f2c22408
neuma573 <crye@kakao.com>
2024-01-21 05:35:33 +0900

    feat: 글쓰기 페이지 엔드포인트 추가

--- src/main/java/com/neuma573/autoboard/mvc/controller/MvcController.java
+++ src/main/java/com/neuma573/autoboard/mvc/controller/MvcController.java
@@ -1,11 +1,23 @@
 package com.neuma573.autoboard.mvc.controller;
 
+import com.neuma573.autoboard.board.service.BoardService;
+import com.neuma573.autoboard.security.utils.JwtProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class MvcController {
+
+    private final JwtProvider jwtProvider;
+
+    private final BoardService boardService;
 
     @GetMapping("/login")
     public ModelAndView showLoginForm() {
@@ -20,6 +32,19 @@ public class MvcController {
     @GetMapping("/join")
     public ModelAndView showJoin() {
         return new ModelAndView("join");
+    }
+
+    @GetMapping("/write")
+    public ModelAndView showWriteForm(@RequestParam(name = "boardId") Long boardId, HttpServletRequest httpServletRequest) {
+        String email = jwtProvider.parseEmailWithValidation(httpServletRequest);
+        if(boardService.checkAccessible(boardId, email)) {
+            ModelAndView modelAndView = new ModelAndView("write");
+            modelAndView.addObject("boardInfo", boardService.getBoardInfo(boardId));
+            return modelAndView;
+        } else {
+            return new ModelAndView("error/error");
+        }
+
     }
 
 }
