37335b5bb794073f9a9905426e2f6483befda2a5
neuma573 <crye@kakao.com>
2024-01-23 19:14:20 +0900

    feat: 조건식 오류 수정

--- src/main/java/com/neuma573/autoboard/board/repository/BoardCustomRepositoryImpl.java
+++ src/main/java/com/neuma573/autoboard/board/repository/BoardCustomRepositoryImpl.java
@@ -2,7 +2,6 @@ package com.neuma573.autoboard.board.repository;
 
 import com.neuma573.autoboard.board.model.entity.Board;
 import com.neuma573.autoboard.board.model.entity.QBoard;
-import com.neuma573.autoboard.post.model.entity.QPost;
 import com.neuma573.autoboard.user.model.entity.User;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -20,18 +19,15 @@ public class BoardCustomRepositoryImpl implements BoardCustomRepository{
     @Override
     public List<Board> findPublicAndNotDeletedBoardWith(Optional<User> userOptional) {
         QBoard qBoard = QBoard.board;
-        QPost qPost = QPost.post;
 
         BooleanBuilder conditions = new BooleanBuilder()
                 .and(qBoard.isDeleted.isFalse())
-                .and(qBoard.isPublic.isTrue())
-                .and(qPost.isDeleted.isFalse());
+                .and(qBoard.isPublic.isTrue());
 
-        userOptional.ifPresent(user -> conditions.and(qBoard.users.contains(user)));
+        userOptional.ifPresent(user -> conditions.or(qBoard.users.contains(user)));
 
         return jpaQueryFactory
                 .selectFrom(qBoard)
-                .leftJoin(qBoard.posts, qPost)
                 .where(conditions)
                 .fetch();
     }
