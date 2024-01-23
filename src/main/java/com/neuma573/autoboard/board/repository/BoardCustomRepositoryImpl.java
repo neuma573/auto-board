b0ce9dba46f9fa953a2180263bd9263f0c6d9160
neuma573 <crye@kakao.com>
2024-01-23 18:36:34 +0900

    feat: left join하여 삭제된 게시글 카운트를 빼보려고 했지만, LAZY 로딩 이슈로 이 방법을 사용할 수 없는 것으로 확인했음.. ORM에서 해결할 수 있는 방법이 있는지 조사 요망

--- src/main/java/com/neuma573/autoboard/board/repository/BoardCustomRepositoryImpl.java
+++ src/main/java/com/neuma573/autoboard/board/repository/BoardCustomRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.neuma573.autoboard.board.repository;
 
 import com.neuma573.autoboard.board.model.entity.Board;
 import com.neuma573.autoboard.board.model.entity.QBoard;
+import com.neuma573.autoboard.post.model.entity.QPost;
 import com.neuma573.autoboard.user.model.entity.User;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -19,14 +20,18 @@ public class BoardCustomRepositoryImpl implements BoardCustomRepository{
     @Override
     public List<Board> findPublicAndNotDeletedBoardWith(Optional<User> userOptional) {
         QBoard qBoard = QBoard.board;
+        QPost qPost = QPost.post;
 
-        BooleanBuilder conditions = new BooleanBuilder(qBoard.isDeleted.isFalse())
-                .and(qBoard.isPublic.isTrue());
+        BooleanBuilder conditions = new BooleanBuilder()
+                .and(qBoard.isDeleted.isFalse())
+                .and(qBoard.isPublic.isTrue())
+                .and(qPost.isDeleted.isFalse());
 
-        userOptional.ifPresent(user -> conditions.or(qBoard.users.contains(user)));
+        userOptional.ifPresent(user -> conditions.and(qBoard.users.contains(user)));
 
         return jpaQueryFactory
                 .selectFrom(qBoard)
+                .leftJoin(qBoard.posts, qPost)
                 .where(conditions)
                 .fetch();
     }
