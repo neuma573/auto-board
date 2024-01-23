a2aec19212ddbff219943281a7fcc882d4e6661d
neuma573 <crye@kakao.com>
2024-01-23 10:49:54 +0900

    feat: 게시글 dto Validation 메세지 추가

--- src/main/java/com/neuma573/autoboard/post/model/dto/PostRequest.java
+++ src/main/java/com/neuma573/autoboard/post/model/dto/PostRequest.java
@@ -5,21 +5,22 @@ import com.neuma573.autoboard.post.model.entity.Post;
 import com.neuma573.autoboard.user.model.entity.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class PostRequest {
 
-    @NotBlank
-    @Size(min=1, max=80)
+    @NotBlank(message = "제목은 비워 둘 수 없습니다")
+    @Size(min = 1, max = 80, message = "제목은 1자 이상 80자 이하이어야 합니다")
     private String title;
 
-    @NotEmpty
-    @Size(min=1, max=1000)
+    @NotEmpty(message = "내용은 비워 둘 수 없습니다")
+    @Size(min = 1, max = 1000, message = "내용은 1자 이상 1000자 이하이어야 합니다")
     private String content;
 
-    @NotEmpty
+    @NotNull
     private Long boardId;
 
     public Post of(Board board, User user) {
