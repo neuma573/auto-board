1833610015bf2d866fd736d90be890e4f1fed9f4
neuma573 <crye@kakao.com>
2024-01-25 22:28:04 +0900

    refactor: 게시판(Board) 서비스 리팩토링 -중복부분 공통화 -aop 권한 어노테이션 적용 -변수 명명 규칙 통일 -메서드는 예외를 던지는 것과 아닌 것으로 분리 개발 -aop layer(BoardAccessAspect)에서 참조하는 게시판 접근 로직 정리

--- src/main/java/com/neuma573/autoboard/board/service/BoardService.java
+++ src/main/java/com/neuma573/autoboard/board/service/BoardService.java
@@ -3,20 +3,19 @@ package com.neuma573.autoboard.board.service;
 import com.neuma573.autoboard.board.model.dto.BoardRequest;
 import com.neuma573.autoboard.board.model.dto.BoardResponse;
 import com.neuma573.autoboard.board.model.entity.Board;
+import com.neuma573.autoboard.board.model.enums.BoardAction;
 import com.neuma573.autoboard.board.repository.BoardRepository;
-import com.neuma573.autoboard.global.exception.AccessDeniedException;
-import com.neuma573.autoboard.global.exception.BoardNotAccessibleException;
 import com.neuma573.autoboard.global.exception.BoardNotFoundException;
-import com.neuma573.autoboard.global.exception.UserNotFoundException;
+import com.neuma573.autoboard.security.model.annotation.CheckPermission;
 import com.neuma573.autoboard.user.model.entity.User;
-import com.neuma573.autoboard.user.repository.UserRepository;
+import com.neuma573.autoboard.user.model.enums.Role;
+import com.neuma573.autoboard.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -27,73 +26,100 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Transactional
-    public List<BoardResponse> getBoardList(Long id) {
-        Optional<User> userOptional = userRepository.findById(id);
-        List<Board> boards;
+    public List<BoardResponse> getBoardList(Long userId) {
 
-        if (userOptional.isPresent() && userOptional.get().isAdmin()) {
-            Iterable<Board> boardIterable = boardRepository.findAll();
-            boards = StreamSupport.stream(boardIterable.spliterator(), false)
-                    .collect(Collectors.toList());
-            return boards.stream()
-                    .map(BoardResponse::of)
-                    .collect(Collectors.toList());
-
+        User user = userService.getUserByIdSafely(userId);
+        if (user != null && userService.isAdmin(user)) {
+            return getBoardListForAdmin();
         } else {
-            boards = boardRepository.findPublicAndNotDeletedBoardWith(userOptional);
-
-            return boards.stream()
-                    .map(BoardResponse::ofWithoutDeleted)
-                    .collect(Collectors.toList());
+            return getBoardListForUser(user);
         }
     }
 
+    @CheckPermission(role = Role.ADMIN)
+    public List<BoardResponse> getBoardListForAdmin() {
+        Iterable<Board> boardIterable = boardRepository.findAll();
+        List<Board> boards = StreamSupport.stream(boardIterable.spliterator(), false)
+                .toList();
+        return boards.stream()
+                .map(BoardResponse::of)
+                .collect(Collectors.toList());
+    }
+
+
+    public List<BoardResponse> getBoardListForUser(User user) {
+        List<Board> boards = boardRepository.findPublicAndNotDeletedBoardWith(user);
+
+        return boards.stream()
+                .map(BoardResponse::ofWithoutDeleted)
+                .collect(Collectors.toList());
+    }
+
+
     @Transactional
-    public BoardResponse saveBoard(Long id, BoardRequest boardRequest) {
-        User user = Optional.ofNullable(id)
-                .flatMap(userRepository::findById)
-                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
-
-        if (!user.isAdmin()) {
-            throw new AccessDeniedException();
-        }
-
+    public BoardResponse saveBoard(Long userId, BoardRequest boardRequest) {
+        User user = userService.getUserById(userId);
         return BoardResponse.of(boardRepository.save(
                 boardRequest.toEntity(
-                user
-                )
+                user)
         ));
     }
 
     @Transactional
-    public boolean checkAccessible(Long boardId, Long userId) {
-        Board board  = boardRepository.findById(boardId).orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시판입니다."));
-        if (userId == -1L) {
-            return board.isPublic();
-        }
-
-        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("존재하지 않는 유저입니다."));
-
-        return board.isAccessible(user);
-    }
-
-    @Transactional
     public BoardResponse getBoardInfo(Long boardId) {
-        return BoardResponse.of(boardRepository.findById(boardId).orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시판입니다.")));
+        return BoardResponse.of(getBoardById(boardId));
     }
 
     @Transactional
-    public void checkAccessibleAndThrow(Long boardId, Long userId) {
-        if (userId != -1L && !checkAccessible(boardId, userId)) {
-            throw new BoardNotAccessibleException("접근할 수 없는 게시판입니다.");
-        }
+    public Board getBoardById(Long boardId) {
+        return boardRepository.findById(boardId).orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시판입니다."));
+    }
 
-        if (!checkAccessible(boardId, userId)) {
-            throw new BoardNotAccessibleException("접근할 수 없는 게시판입니다.");
-        }
+    @Transactional
+    public void deleteBoard(Long boardId) {
+        getBoardById(boardId).delete();
+    }
+
+    @Transactional
+    public void modifyBoard(BoardRequest boardRequest) {
+        Board board = getBoardById(boardRequest.getId());
+        modify(board, boardRequest);
+    }
+
+    public void modify(Board board, BoardRequest boardRequest) {
+        board.setName(boardRequest.getName());
+    }
+
+    public boolean isContainedUser(User user, Long boardId) {
+        return boardRepository.findByUsersAndId(user, boardId).isPresent();
+    }
+
+    @Transactional
+    public boolean isCreatable(Long userId) {
+        return userService.isAdmin(
+                userService.getUserById(userId)
+        );
+    }
+
+    @Transactional
+    public boolean isBoardAccessible(Long userId, Long boardId, BoardAction action) {
+        Board board = getBoardById(boardId);
+        User user = userService.getUserByIdSafely(userId);
+
+        return switch (action) {
+            case READ -> {
+                if (user == null) {
+                    yield board.isPublic() && !board.isDeleted();
+                }
+                yield userService.isAdmin(user) || (!board.isDeleted() && (board.isPublic() || isContainedUser(user, boardId)));
+            }
+            case DELETE -> userService.isAdmin(user) && !board.isDeleted();
+            case UPDATE, CREATE -> userService.isAdmin(user);
+            default -> false;
+        };
     }
 
 }
