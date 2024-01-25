44786e7c3c103bf78286377f8a70d05cc1a1d8a9
neuma573 <crye@kakao.com>
2024-01-25 22:15:04 +0900

    feat: 게시판(Board) CRUD 작업 시 사용자의 접근 권한을 확인하는 AOP 로직 및 커스텀 어노테이션을 구현

---
+++ src/main/java/com/neuma573/autoboard/board/aop/BoardAccessAspect.java
@@ -0,0 +1,74 @@
+package com.neuma573.autoboard.board.aop;
+
+import com.neuma573.autoboard.board.model.annotation.CheckBoardAccess;
+import com.neuma573.autoboard.board.model.enums.BoardAction;
+import com.neuma573.autoboard.board.service.BoardService;
+import com.neuma573.autoboard.global.exception.BoardNotAccessibleException;
+import com.neuma573.autoboard.post.service.PostService;
+import com.neuma573.autoboard.security.utils.JwtProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Objects;
+
+@Aspect
+@RequiredArgsConstructor
+@Component
+public class BoardAccessAspect {
+
+    private final JwtProvider jwtProvider;
+
+    private final BoardService boardService;
+
+    private final PostService postService;
+
+    @Around("@annotation(checkBoardAccess)")
+    public Object checkUserBoardAccess(ProceedingJoinPoint joinPoint, CheckBoardAccess checkBoardAccess) throws Throwable {
+        BoardAction action = checkBoardAccess.action();
+        HttpServletRequest httpServletRequest = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+        Long userId = jwtProvider.parseUserIdSafely(httpServletRequest);
+        Long boardId = checkBoardAccess.boardId();
+        Long postId = checkBoardAccess.postId();
+        Object[] args = joinPoint.getArgs();
+
+        for (Object arg : args) {
+            if (arg instanceof Long) {
+                boardId = (Long) arg;
+                break;
+            }
+        }
+
+        if (boardId == -1 && postId != -1) {
+            boardId = postService.findBoardIdByPostId(postId);
+        }
+
+        switch (action) {
+            case READ -> {
+                if (!boardService.isBoardAccessible(userId, boardId, BoardAction.READ)) {
+                    throw new BoardNotAccessibleException("게시판 읽기 권한이 없습니다.");
+                }
+            }
+            case DELETE -> {
+                if (!boardService.isBoardAccessible(userId, boardId, action)) {
+                    throw new BoardNotAccessibleException("게시판 삭제 권한이 없습니다.");
+                }
+            }
+            case CREATE, UPDATE -> {
+                if (!boardService.isCreatable(userId)) {
+                    throw new BoardNotAccessibleException("게시판 생성/수정 권한이 없습니다.");
+                }
+            }
+            case UNAUTHORIZED_ACTION -> {
+
+            }
+        }
+
+        return joinPoint.proceed();
+    }
+}
---
+++ src/main/java/com/neuma573/autoboard/board/model/annotation/CheckBoardAccess.java
@@ -0,0 +1,17 @@
+package com.neuma573.autoboard.board.model.annotation;
+
+import com.neuma573.autoboard.board.model.enums.BoardAction;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CheckBoardAccess {
+    BoardAction action();
+
+    long boardId() default -1;
+    long postId() default -1;
+}
