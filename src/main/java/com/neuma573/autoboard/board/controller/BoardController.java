acc5fbb02db2a1ea3153dc42d88101a258df211f
neuma573 <crye@kakao.com>
2024-01-25 22:28:55 +0900

    feat: 게시판 삭제/수정 api 작성

--- src/main/java/com/neuma573/autoboard/board/controller/BoardController.java
+++ src/main/java/com/neuma573/autoboard/board/controller/BoardController.java
@@ -1,11 +1,12 @@
 package com.neuma573.autoboard.board.controller;
 
+import com.neuma573.autoboard.board.model.annotation.CheckBoardAccess;
 import com.neuma573.autoboard.board.model.dto.BoardRequest;
 import com.neuma573.autoboard.board.model.dto.BoardResponse;
+import com.neuma573.autoboard.board.model.enums.BoardAction;
 import com.neuma573.autoboard.board.service.BoardService;
 import com.neuma573.autoboard.global.model.dto.Response;
 import com.neuma573.autoboard.global.utils.ResponseUtils;
-import com.neuma573.autoboard.security.model.annotation.CheckPermission;
 import com.neuma573.autoboard.security.utils.JwtProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -30,16 +31,31 @@ public class BoardController {
 
     private final ResponseUtils responseUtils;
 
+    @CheckBoardAccess(action = BoardAction.READ)
     @GetMapping("")
     public ResponseEntity<Response<List<BoardResponse>>> getBoardList(HttpServletRequest httpServletRequest) {
-        Long userId = jwtProvider.parseIdFrom(httpServletRequest);
+        Long userId = jwtProvider.parseUserIdSafely(httpServletRequest);
         return ResponseEntity.ok().body(responseUtils.success(boardService.getBoardList(userId)));
     }
 
-    @CheckPermission("ADMIN")
+    @CheckBoardAccess(action = BoardAction.CREATE)
     @PostMapping("")
     public ResponseEntity<Response<BoardResponse>> saveBoard(@Valid @RequestBody BoardRequest boardRequest, HttpServletRequest httpServletRequest) {
-        Long userId = jwtProvider.getUserId(httpServletRequest);
+        Long userId = jwtProvider.parseUserId(httpServletRequest);
         return ResponseEntity.created(URI.create("/main")).body(responseUtils.created(boardService.saveBoard(userId, boardRequest)));
+    }
+
+    @CheckBoardAccess(action = BoardAction.UPDATE)
+    @PutMapping("")
+    public ResponseEntity<Void> modifyBoard(@Valid @RequestBody BoardRequest boardRequest) {
+        boardService.modifyBoard(boardRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+    @CheckBoardAccess(action = BoardAction.DELETE)
+    @DeleteMapping("")
+    public ResponseEntity<Void> deleteBoard(@RequestParam(name = "boardId") Long boardId) {
+        boardService.deleteBoard(boardId);
+        return ResponseEntity.noContent().build();
     }
 }
