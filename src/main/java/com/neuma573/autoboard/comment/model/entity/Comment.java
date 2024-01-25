5b0806cee1c372e36cd8c976a74c12cf8360db5d
neuma573 <crye@kakao.com>
2024-01-25 15:02:28 +0900

    feat: 댓글 entity 작성

--- src/main/java/com/neuma573/autoboard/comment/model/entity/Comment.java
+++ src/main/java/com/neuma573/autoboard/comment/model/entity/Comment.java
@@ -4,8 +4,20 @@ import com.neuma573.autoboard.global.model.entity.BaseEntity;
 import com.neuma573.autoboard.post.model.entity.Post;
 import com.neuma573.autoboard.user.model.entity.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Builder
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 @Table(name = "comment")
 public class Comment extends BaseEntity {
 
@@ -16,11 +28,19 @@ public class Comment extends BaseEntity {
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "created_user_id")
     private User createdBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", fetch = FetchType.LAZY)
+    private List<Comment> replies = new ArrayList<>();
+
 }
