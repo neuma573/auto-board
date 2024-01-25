1a96b4030d089a1e68ba581069cf252158b431aa
neuma573 <crye@kakao.com>
2024-01-25 22:20:01 +0900

    feat: 유저가 게시판 이용 목록에 들어가있는지 여부를 판단하기 위해 쿼리메서드 작성

--- src/main/java/com/neuma573/autoboard/board/repository/BoardRepository.java
+++ src/main/java/com/neuma573/autoboard/board/repository/BoardRepository.java
@@ -1,7 +1,13 @@
 package com.neuma573.autoboard.board.repository;
 
 import com.neuma573.autoboard.board.model.entity.Board;
+import com.neuma573.autoboard.user.model.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface BoardRepository extends CrudRepository<Board, Long>, BoardCustomRepository {
+
+    Optional<Board> findByUsersAndId(User user, Long id);
+
 }
