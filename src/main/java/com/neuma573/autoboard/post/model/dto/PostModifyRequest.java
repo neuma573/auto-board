c9fe9b06c8608e424186b31b9995b2c7300757ab
neuma573 <crye@kakao.com>
2024-01-27 18:27:07 +0900

    refactor: 상속관계 구현

--- src/main/java/com/neuma573/autoboard/post/model/dto/PostModifyRequest.java
+++ src/main/java/com/neuma573/autoboard/post/model/dto/PostModifyRequest.java
@@ -6,15 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
-public class PostModifyRequest {
+public class PostModifyRequest extends PostRequest {
 
     private Long postId;
-
-    @NotBlank(message = "제목은 비워 둘 수 없습니다")
-    @Size(min = 1, max = 80, message = "제목은 1자 이상 80자 이하이어야 합니다")
-    private String title;
-
-    @NotEmpty(message = "내용은 비워 둘 수 없습니다")
-    @Size(min = 1, max = 1000, message = "내용은 1자 이상 1000자 이하이어야 합니다")
-    private String content;
 }
