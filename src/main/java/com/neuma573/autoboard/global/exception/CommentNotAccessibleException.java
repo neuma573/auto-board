609edca329bf5b25508f1cf8086eedbb80319538
neuma573 <crye@kakao.com>
2024-01-30 01:47:44 +0900

    feat: 댓글 접근 불가 커스텀 예외 추가

---
+++ src/main/java/com/neuma573/autoboard/global/exception/CommentNotAccessibleException.java
@@ -0,0 +1,8 @@
+package com.neuma573.autoboard.global.exception;
+
+public class CommentNotAccessibleException extends RuntimeException {
+
+    public CommentNotAccessibleException(String message) {
+        super(message);
+    }
+}
