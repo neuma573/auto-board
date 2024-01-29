a182689b5932eb2f1dc03422165dbf4cd825500a
neuma573 <crye@kakao.com>
2024-01-30 01:47:10 +0900

    feat: not-null validation 추가

--- src/main/java/com/neuma573/autoboard/post/model/dto/PostModifyRequest.java
+++ src/main/java/com/neuma573/autoboard/post/model/dto/PostModifyRequest.java
@@ -1,12 +1,11 @@
 package com.neuma573.autoboard.post.model.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class PostModifyRequest extends PostRequest {
 
+    @NotNull
     private Long postId;
 }
