112b1b5060c361f8746f3d0ba936db37109863eb
neuma573 <crye@kakao.com>
2024-01-31 15:52:19 +0900

    fix: 스페이스 입력 가능하던 문제 해결

--- src/main/java/com/neuma573/autoboard/post/model/dto/PostRequest.java
+++ src/main/java/com/neuma573/autoboard/post/model/dto/PostRequest.java
@@ -4,8 +4,6 @@ import com.neuma573.autoboard.board.model.entity.Board;
 import com.neuma573.autoboard.post.model.entity.Post;
 import com.neuma573.autoboard.user.model.entity.User;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
@@ -16,7 +14,7 @@ public class PostRequest {
     @Size(min = 1, max = 80, message = "제목은 1자 이상 80자 이하이어야 합니다")
     private String title;
 
-    @NotEmpty(message = "내용은 비워 둘 수 없습니다")
+    @NotBlank(message = "내용은 비워 둘 수 없습니다")
     @Size(min = 1, max = 1000, message = "내용은 1자 이상 1000자 이하이어야 합니다")
     private String content;
 
