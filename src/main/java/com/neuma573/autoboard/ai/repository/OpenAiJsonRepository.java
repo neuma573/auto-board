18b4ab988e6b2785142c1395a87ccc53a9045db8
neuma573 <crye@kakao.com>
2024-01-31 12:32:02 +0900

    feat: OpenAi dto 작성

---
+++ src/main/java/com/neuma573/autoboard/ai/repository/OpenAiJsonRepository.java
@@ -0,0 +1,7 @@
+package com.neuma573.autoboard.ai.repository;
+
+import com.neuma573.autoboard.ai.model.entity.OpenAiJson;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OpenAiJsonRepository extends CrudRepository<OpenAiJson, Long> {
+}
