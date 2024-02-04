76c11716cd155aaeef863d7b7a96bd0cad03680f
neuma573 <crye@kakao.com>
2024-02-04 20:16:39 +0900

    feat: User 엔티티와 일대일 연관관계 설정

--- src/main/java/com/neuma573/autoboard/ai/model/entity/Bot.java
+++ src/main/java/com/neuma573/autoboard/ai/model/entity/Bot.java
@@ -1,14 +1,28 @@
 package com.neuma573.autoboard.ai.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.neuma573.autoboard.global.model.entity.BaseEntity;
+import com.neuma573.autoboard.user.model.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Entity
-public class Bot {
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "bot")
+public class Bot extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bot_user_id")
+    private User bot;
 }
