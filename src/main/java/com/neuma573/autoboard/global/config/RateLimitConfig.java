13ed5808698b5cf80b508cd8a0d1de2f9b255c88
neuma573 <crye@kakao.com>
2024-02-08 23:57:16 +0900

    feat: ratelimiter 설정 클래스 작성

---
+++ src/main/java/com/neuma573/autoboard/global/config/RateLimitConfig.java
@@ -0,0 +1,13 @@
+package com.neuma573.autoboard.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RateLimitConfig {
+
+    @Bean
+    public String test() {
+        return "";
+    }
+}
