edb98da49ec4459d91e9be483c71c77b09530b1d
neuma573 <crye@kakao.com>
2024-02-19 10:41:18 +0900

    fix : 요청 초과 예외 기본 메세지 작성

--- src/main/java/com/neuma573/autoboard/global/exception/RateLimitExceededException.java
+++ src/main/java/com/neuma573/autoboard/global/exception/RateLimitExceededException.java
@@ -3,7 +3,7 @@ package com.neuma573.autoboard.global.exception;
 public class RateLimitExceededException extends RuntimeException {
 
     public RateLimitExceededException() {
-        super();
+        super("너무 많은 요청을 할 수 없습니다");
     }
 
     public RateLimitExceededException(String message) {
