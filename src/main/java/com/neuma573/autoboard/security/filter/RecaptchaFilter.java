5fbb6b818aa024e1a281a3ef739df73ae6adb977
neuma573 <crye@kakao.com>
2024-02-25 03:36:13 +0900

    fix : filter layer에서 예외를 캐치하려는 시도는 Filter 내에서의 캐치로 변경됨 ResponseUtils에서의 로깅 통일, 상태코드 입력을 유동적으로 변경

--- src/main/java/com/neuma573/autoboard/security/filter/RecaptchaFilter.java
+++ src/main/java/com/neuma573/autoboard/security/filter/RecaptchaFilter.java
@@ -1,6 +1,10 @@
 package com.neuma573.autoboard.security.filter;
 
+import com.neuma573.autoboard.global.exception.ExceptionCode;
+import com.neuma573.autoboard.global.exception.RecaptchaValidationException;
+
 import com.neuma573.autoboard.global.utils.RequestUtils;
+import com.neuma573.autoboard.global.utils.ResponseUtils;
 import com.neuma573.autoboard.security.service.RecaptchaService;
 import com.neuma573.autoboard.security.utils.UrlPatternManager;
 import jakarta.servlet.FilterChain;
@@ -15,6 +19,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -22,15 +27,21 @@ public class RecaptchaFilter extends OncePerRequestFilter {
 
     private final RecaptchaService recaptchaService;
     private final UrlPatternManager urlPatternManager;
-
+    private final ResponseUtils responseUtils;
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         String requestURI = RequestUtils.getRequestUri(httpServletRequest);
         HttpMethod method = RequestUtils.getMethod(httpServletRequest);
-        if (urlPatternManager.isRecaptchaProtectedUrl(requestURI, method)) {
-            log.info("Recaptcha Verification Request URI : [{}] {}", method, requestURI);
-            recaptchaService.createAssessment(httpServletRequest);
+        try {
+            if (urlPatternManager.isRecaptchaProtectedUrl(requestURI, method)) {
+                log.info("Recaptcha Verification Request URI : [{}] {}", method, requestURI);
+                recaptchaService.createAssessment(httpServletRequest);
+            }
+        } catch (RecaptchaValidationException ex) {
+            responseUtils.setResponse(httpServletResponse, ExceptionCode.ILLEGAL_RECAPTCHA_REQUEST, ex);
+            return;
         }
+
 
         filterChain.doFilter(httpServletRequest, httpServletResponse); // 검증 성공시 다음 필터로 요청 전달
     }
