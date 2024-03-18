49ca872e4646f16653c2724f7b5233b1d50a89a5
neuma573 <crye@kakao.com>
2024-03-18 14:02:01 +0900

    remove: import문 최적화

--- src/main/java/com/neuma573/autoboard/user/controller/UserController.java
+++ src/main/java/com/neuma573/autoboard/user/controller/UserController.java
@@ -12,7 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
