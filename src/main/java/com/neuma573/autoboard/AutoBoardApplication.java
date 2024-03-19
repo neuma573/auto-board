8f7379721e5204a599f294f06cc5d900b477895d
neuma573 <crye@kakao.com>
2024-03-19 15:54:07 +0900

    feat: feignClient를 사용하기 위해 루트 어플리케이션에 어노테이션 추가

--- src/main/java/com/neuma573/autoboard/AutoBoardApplication.java
+++ src/main/java/com/neuma573/autoboard/AutoBoardApplication.java
@@ -2,6 +2,7 @@ package com.neuma573.autoboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableRedisRepositories
 @EnableAsync
 @EnableScheduling
+@EnableFeignClients
 @SpringBootApplication
 public class AutoBoardApplication {
 
