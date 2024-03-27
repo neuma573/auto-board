b5c351256436cc334654c0aa3c2511bda8494cc5
neuma573 <crye@kakao.com>
2024-03-27 22:19:52 +0900

    feat: OAuth Callback 을 핸들링할 Service 작성 -네이버, Google * 토큰 요청 및 유저 정보 요청 서비스 구현

---
+++ src/main/java/com/neuma573/autoboard/user/service/OAuthService.java
@@ -0,0 +1,82 @@
+package com.neuma573.autoboard.user.service;
+
+import com.neuma573.autoboard.global.client.GoogleAuthClient;
+import com.neuma573.autoboard.global.client.GoogleUserClient;
+import com.neuma573.autoboard.global.client.NaverAuthClient;
+import com.neuma573.autoboard.global.client.NaverUserClient;
+import com.neuma573.autoboard.user.model.dto.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class OAuthService {
+
+    private final NaverAuthClient naverAuthClient;
+
+    private final NaverUserClient naverUserClient;
+
+    private final GoogleAuthClient googleAuthClient;
+
+    private final GoogleUserClient googleUserClient;
+
+    @Value("${app.oauth2.naver.client-id}")
+    private String naverClientId;
+
+    @Value("${app.oauth2.naver.client-secret}")
+    private String naverClientSecret;
+
+    @Value("${app.oauth2.google.client-id}")
+    private String googleClientId;
+
+    @Value("${app.oauth2.google.client-secret}")
+    private String googleClientSecret;
+
+    @Value("${app.domain}")
+    private String domain;
+
+    public NaverUserResponse getAuthenticate(String code, String state) {
+        NaverTokenResponse tokenResponse = getToken(code, state);
+        return getNaverUserResponse(tokenResponse);
+    }
+
+    public GoogleUserResponse getAuthenticate(String code, String state, String scope) {
+        GoogleTokenResponse tokenResponse = getToken(code, state, domain + "/api/v1/oauth2/google/callback");
+        return getGoogleUserResponse(tokenResponse);
+    }
+
+    private NaverTokenResponse getToken(String code, String state) {
+        return naverAuthClient.getToken(
+                "authorization_code",
+                naverClientId,
+                naverClientSecret,
+                code,
+                state
+        );
+    }
+
+    private GoogleTokenResponse getToken(String code, String state, String redirectUri) {
+        return googleAuthClient.getToken(
+                code,
+                googleClientId,
+                googleClientSecret,
+                redirectUri,
+                "authorization_code"
+        );
+    }
+
+    private NaverUserResponse getNaverUserResponse(NaverTokenResponse tokenResponse) {
+        return naverUserClient.getNaverUserResponse(getAuthorizationHeader(tokenResponse));
+    }
+
+    private GoogleUserResponse getGoogleUserResponse(GoogleTokenResponse tokenResponse) {
+        return googleUserClient.getGoogleUserResponse(getAuthorizationHeader(tokenResponse));
+    }
+
+    private String getAuthorizationHeader(TokenResponse tokenResponse) {
+        return tokenResponse.getTokenType() + " " + tokenResponse.getAccessToken();
+    }
+}
