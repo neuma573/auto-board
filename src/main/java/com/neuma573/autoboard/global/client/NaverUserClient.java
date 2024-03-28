0dc35bf0b2a21a1bdcee7da785413ba78d41d921
neuma573 <crye@kakao.com>
2024-03-28 19:03:32 +0900

    refactor: 사용하지 않는 import 문 삭제

--- src/main/java/com/neuma573/autoboard/global/client/NaverUserClient.java
+++ src/main/java/com/neuma573/autoboard/global/client/NaverUserClient.java
@@ -3,7 +3,6 @@ package com.neuma573.autoboard.global.client;
 import com.neuma573.autoboard.user.model.dto.NaverUserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "NaverUserClient", url = "https://openapi.naver.com")
