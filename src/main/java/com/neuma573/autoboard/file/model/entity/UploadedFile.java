5a83c32a066d03133615889e0e0ba1e4bcba6070
neuma573 <crye@kakao.com>
2024-04-04 11:17:25 +0900

    feat: 파일 업로드에 사용할 Entity 작성

---
+++ src/main/java/com/neuma573/autoboard/file/model/entity/UploadedFile.java
@@ -0,0 +1,50 @@
+package com.neuma573.autoboard.file.model.entity;
+
+import com.neuma573.autoboard.global.model.entity.BaseEntity;
+import com.neuma573.autoboard.post.model.entity.Post;
+import com.neuma573.autoboard.user.model.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "uploaded_file")
+public class UploadedFile extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String originalFileName;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @Column(nullable = false)
+    private String contentType;
+
+    @Column(nullable = false)
+    private Long size;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_user_id")
+    private User createdBy;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+}
