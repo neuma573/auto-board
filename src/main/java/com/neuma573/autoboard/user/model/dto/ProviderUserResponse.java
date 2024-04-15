d9e31ac61a7525011538ed596c68f7cf6e46d0a3
neuma573 <crye@kakao.com>
2024-04-15 21:13:10 +0900

    feat: Provider 정보를 담은 Response DTO에 Provider 유형 필드 추가

--- src/main/java/com/neuma573/autoboard/user/model/dto/ProviderUserResponse.java
+++ src/main/java/com/neuma573/autoboard/user/model/dto/ProviderUserResponse.java
@@ -1,14 +1,21 @@
 package com.neuma573.autoboard.user.model.dto;
 
+import com.neuma573.autoboard.user.model.enums.AuthenticationProviderType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class ProviderUserResponse {
 
     String providerId;
 
     String email;
+
+    AuthenticationProviderType authenticationProviderType;
 
 }
