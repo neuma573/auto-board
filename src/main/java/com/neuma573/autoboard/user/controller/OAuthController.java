bc7892ad67ed29a2bb9350321eec3397d60a105e
neuma573 <crye@kakao.com>
2024-04-15 21:11:42 +0900

    feat: NAVER / Google OAuth 구현을 위한 콜백 api 수정, 임시 가입 처리 후 가입 페이지로 리다이렉션

--- src/main/java/com/neuma573/autoboard/user/controller/OAuthController.java
+++ src/main/java/com/neuma573/autoboard/user/controller/OAuthController.java
@@ -1,14 +1,18 @@
 package com.neuma573.autoboard.user.controller;
 
-import com.neuma573.autoboard.global.model.dto.Response;
-import com.neuma573.autoboard.global.utils.ResponseUtils;
 import com.neuma573.autoboard.user.model.dto.GoogleUserResponse;
 import com.neuma573.autoboard.user.model.dto.NaverUserResponse;
+import com.neuma573.autoboard.user.model.enums.AuthenticationProviderType;
 import com.neuma573.autoboard.user.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RequestMapping("/api/v1/oauth2")
 @RequiredArgsConstructor
@@ -16,25 +20,49 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class OAuthController {
 
-    private final ResponseUtils responseUtils;
-
     private final OAuthService oAuthService;
 
     @GetMapping("/naver/callback")
-    public ResponseEntity<Response<NaverUserResponse>> getNaverOAuthCallback(
+    public ResponseEntity<Void> getNaverOAuthCallback(
             @RequestParam(value = "code") String code,
-            @RequestParam(value = "state") String state)
+            @RequestParam(value = "state") String state
+    )
     {
-        return ResponseEntity.ok().body(responseUtils.created(oAuthService.getAuthenticate(code, state)));
+        NaverUserResponse naverUserResponse = oAuthService.getAuthenticate(code, state);
+        return handleLoginAndRedirect(
+                naverUserResponse.getId(),
+                naverUserResponse.getEmail(),
+                AuthenticationProviderType.NAVER
+        );
     }
 
     @GetMapping("/google/callback")
-    public ResponseEntity<Response<GoogleUserResponse>> getGoogleOAuthCallback(
+    public ResponseEntity<Void> getGoogleOAuthCallback(
             @RequestParam(value = "code") String code
     )
     {
-        return ResponseEntity.ok().body(responseUtils.created(oAuthService.getAuthenticate(code)));
+        GoogleUserResponse googleUserResponse = oAuthService.getAuthenticate(code);
+        return handleLoginAndRedirect(
+                googleUserResponse.getId(),
+                googleUserResponse.getEmail(),
+                AuthenticationProviderType.GOOGLE
+        );
     }
 
+    private ResponseEntity<Void> handleLoginAndRedirect(
+            String providerId,
+            String email,
+            AuthenticationProviderType authenticationProviderType
+    )
+    {
+        String joinUuid = oAuthService.handleOAuthLogin(providerId, email, authenticationProviderType);
+        if (!joinUuid.isEmpty()) {
+            String encodedUuid = URLEncoder.encode(joinUuid, StandardCharsets.UTF_8);
+            URI joinUri = URI.create("/oauth/join?code=" + encodedUuid);
+            return ResponseEntity.status(HttpStatus.SEE_OTHER).location(joinUri).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.SEE_OTHER).location(URI.create("/main")).build();
+        }
+    }
 }
 
