88d55e4484dc74d2027145d332f8f83edb3e0e1a
neuma573 <crye@kakao.com>
2024-04-16 17:50:54 +0900

    refactor: jwt 갱신은 컨트롤러 레벨에서 하도록 리팩토링

--- src/main/java/com/neuma573/autoboard/security/service/TokenService.java
+++ src/main/java/com/neuma573/autoboard/security/service/TokenService.java
@@ -1,25 +1,16 @@
 package com.neuma573.autoboard.security.service;
 
 import com.neuma573.autoboard.security.model.dto.AccessTokenRequest;
-import com.neuma573.autoboard.security.model.dto.AccessTokenResponse;
 import com.neuma573.autoboard.security.utils.JwtProvider;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @RequiredArgsConstructor
 @Service
 public class TokenService {
 
     private final JwtProvider jwtProvider;
-
-    public AccessTokenResponse refreshAccessToken(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
-        return jwtProvider.refreshAccessToken(httpServletRequest, httpServletResponse);
-    }
 
     public boolean validateToken(AccessTokenRequest accessTokenRequest) {
         return jwtProvider.validateAccessTokenWithoutResponse(accessTokenRequest.getAccessToken());
