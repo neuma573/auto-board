4efe41e729d04281ab416d4dc5ccabd27742d72b
neuma573 <crye@kakao.com>
2024-04-16 17:51:00 +0900

    refactor: ClientInfo 초기화는 정적 팩토리 메서드를 통해 진행하여 가독성 향상

--- src/main/java/com/neuma573/autoboard/security/model/dto/ClientInfo.java
+++ src/main/java/com/neuma573/autoboard/security/model/dto/ClientInfo.java
@@ -1,5 +1,7 @@
 package com.neuma573.autoboard.security.model.dto;
 
+import com.neuma573.autoboard.global.utils.RequestUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,5 +12,15 @@ public class ClientInfo {
     private String clientIpAddress;
 
     private String userAgent;
+
+    private String requestUri;
+
+    public static ClientInfo of(HttpServletRequest httpServletRequest) {
+        return ClientInfo.builder()
+                .clientIpAddress(RequestUtils.getClientIpAddress(httpServletRequest))
+                .userAgent(RequestUtils.getUserAgent(httpServletRequest))
+                .requestUri(RequestUtils.getRequestUri(httpServletRequest))
+                .build();
+    }
 
 }
