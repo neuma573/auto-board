752e5adea58243e3df3253ff3b56c266a4c33879
neuma573 <crye@kakao.com>
2024-04-25 13:56:18 +0900

    feat: xss 공격 방지를 위한 Jsoup 도입

---
+++ src/main/java/com/neuma573/autoboard/global/utils/XSSUtils.java
@@ -0,0 +1,24 @@
+package com.neuma573.autoboard.global.utils;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
+
+public class XSSUtils {
+
+    public static String filterHtmlSource(String source) {
+        Safelist safelist = Safelist.basicWithImages();
+
+        safelist.addTags("figure");
+        safelist.addAttributes("figure", "class");
+
+        safelist.addAttributes("img", "style");
+
+        safelist.addTags("iframe");
+        safelist.addAttributes("iframe", "src", "width", "height", "frameborder", "allow", "allowfullscreen");
+
+        safelist.addAttributes("blockquote", "cite");
+
+        return Jsoup.clean(source, safelist);
+    }
+}
