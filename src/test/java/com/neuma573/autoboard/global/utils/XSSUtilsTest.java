6f69d3ca734ac3e0d112cb207d0f5ef0318583a9
neuma573 <crye@kakao.com>
2024-04-26 11:34:56 +0900

    test: Jsoup 필터링 코드 작성

---
+++ src/test/java/com/neuma573/autoboard/global/utils/XSSUtilsTest.java
@@ -0,0 +1,18 @@
+package com.neuma573.autoboard.global.utils;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class XSSUtilsTest {
+
+    @Test
+    public void testFilterHtmlSource() {
+        String htmlSource = "<img src=x onerror=alert(1) style=\"width:0;height:0\">";
+
+        String expected = "<img style=\"width:0;height:0\">";
+
+        String actual = XSSUtils.filterHtmlSource(htmlSource);
+
+        assertEquals(expected, actual, "Filtered HTML should not contain any script tags or styles.");
+    }
+}
