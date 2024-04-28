142666f00b9f5ca78286bb96ecf2ac2b312a6e92
neuma573 <crye@kakao.com>
2024-04-28 13:56:18 +0900

    feat: 게시글 응답 테스트 생성

---
+++ src/test/java/com/neuma573/autoboard/post/service/PostServiceTest.java
@@ -0,0 +1,13 @@
+package com.neuma573.autoboard.post.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @Test
+    void testGetPost() {
+        
+    }
+}
