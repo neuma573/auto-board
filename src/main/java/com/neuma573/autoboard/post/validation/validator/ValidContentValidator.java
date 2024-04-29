5a24a4756deab9867d7d677c4da9b0c7933f927e
neuma573 <crye@kakao.com>
2024-04-29 14:03:35 +0900

    feat: 보이지 않는 문자 제거 및 공백 체크를 포함한 ValidContent 검증기 추가

---
+++ src/main/java/com/neuma573/autoboard/post/validation/validator/ValidContentValidator.java
@@ -0,0 +1,21 @@
+package com.neuma573.autoboard.post.validation.validator;
+
+import com.neuma573.autoboard.post.validation.annotation.ValidContent;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ValidContentValidator implements ConstraintValidator<ValidContent, String> {
+    @Override
+    public void initialize(ValidContent constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return false;
+        }
+        String modifiedValue = value.replace("\u00AD", "");
+        return !modifiedValue.trim().isEmpty();
+    }
+}
