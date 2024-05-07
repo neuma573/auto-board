01f497653055c4d1015dd600f8e7e05b1014cd51
neuma573 <crye@kakao.com>
2024-05-07 13:00:09 +0900

    feat: 좋아요 기능 구현에 사용할 Request 요청 DTO 작성

--- src/main/java/com/neuma573/autoboard/like/model/dto/LikeRequest.java
+++ src/main/java/com/neuma573/autoboard/like/model/dto/LikeRequest.java
@@ -1,9 +1,18 @@
 package com.neuma573.autoboard.like.model.dto;
 
+import com.neuma573.autoboard.like.model.entity.Like;
+import com.neuma573.autoboard.post.model.entity.Post;
+import com.neuma573.autoboard.user.model.entity.User;
 import lombok.Getter;
 
 @Getter
 public class LikeRequest {
     private Long postId;
-    private Long commentId;
+
+    public static Like of(User user, Post post) {
+        return Like.builder()
+                .post(post)
+                .createdBy(user)
+                .build();
+    }
 }
