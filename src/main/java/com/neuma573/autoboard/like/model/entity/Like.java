197437e9953f2ba7323e270581398f0bd9e234ef
neuma573 <crye@kakao.com>
2024-05-07 14:11:20 +0900

    feat: 사용되지 않는 import문 최적화

--- src/main/java/com/neuma573/autoboard/like/model/entity/Like.java
+++ src/main/java/com/neuma573/autoboard/like/model/entity/Like.java
@@ -1,6 +1,5 @@
 package com.neuma573.autoboard.like.model.entity;
 
-import com.neuma573.autoboard.comment.model.entity.Comment;
 import com.neuma573.autoboard.global.model.entity.BaseEntity;
 import com.neuma573.autoboard.post.model.entity.Post;
 import com.neuma573.autoboard.user.model.entity.User;
