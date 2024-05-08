9161d7c04e60f3f06911e2acfdec8df6bdf967f2
neuma573 <crye@kakao.com>
2024-05-08 09:17:08 +0900

    feat: 좋아요 생성 서비스 로직 작성

--- src/main/java/com/neuma573/autoboard/like/service/LikeService.java
+++ src/main/java/com/neuma573/autoboard/like/service/LikeService.java
@@ -1,7 +1,41 @@
 package com.neuma573.autoboard.like.service;
 
+import com.neuma573.autoboard.like.model.dto.LikeRequest;
+import com.neuma573.autoboard.like.model.dto.LikeResponse;
+import com.neuma573.autoboard.like.model.entity.Like;
+import com.neuma573.autoboard.like.repository.LikeRepository;
+import com.neuma573.autoboard.post.model.entity.Post;
+import com.neuma573.autoboard.post.service.PostService;
+import com.neuma573.autoboard.user.model.entity.User;
+import com.neuma573.autoboard.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class LikeService {
+
+    private final LikeRepository likeRepository;
+
+    private final UserService userService;
+
+    private final PostService postService;
+
+    public LikeResponse createLike(LikeRequest likeRequest, Long userId) {
+        User user = userService.getUserById(userId);
+        Post post = postService.getPostById(likeRequest.getPostId());
+        Like like = likeRepository.save(
+                LikeRequest.of(user, post)
+        );
+        return LikeResponse.of(like);
+    }
+
+    public void deleteLike(LikeRequest likeRequest) {
+
+    }
+
+    public Long getLikeCount(LikeRequest likeRequest) {
+        return 0L;
+    }
+
 }
