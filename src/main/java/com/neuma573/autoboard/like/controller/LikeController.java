96857cac95b7f2cf7ea8385bbc678934cebdc5f5
neuma573 <crye@kakao.com>
2024-05-10 13:58:26 +0900

    feat: 좋아요 갯수를 반환하는 엔드포인트 생성

--- src/main/java/com/neuma573/autoboard/like/controller/LikeController.java
+++ src/main/java/com/neuma573/autoboard/like/controller/LikeController.java
@@ -1,13 +1,28 @@
 package com.neuma573.autoboard.like.controller;
 
+import com.neuma573.autoboard.global.model.dto.Response;
+import com.neuma573.autoboard.global.utils.ResponseUtils;
 import com.neuma573.autoboard.like.service.LikeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/likes")
+@RequestMapping("/api/v1/like")
 public class LikeController {
+
     private final LikeService likeService;
+
+    private final ResponseUtils responseUtils;
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<Response<Long>> countLikes(@PathVariable Long postId) {
+//        int likeCount = likeService.countLikes(postId);
+        return ResponseEntity.ok().body(responseUtils.success(0L));
+    }
+
 }
