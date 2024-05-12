cad56f6d342c8ef891a2f9c83b2d2ac74e09dd13
neuma573 <crye@kakao.com>
2024-05-12 13:01:52 +0900

    feat: LikeRequest에 lombok Builder 패턴 적용

--- src/main/java/com/neuma573/autoboard/like/model/dto/LikeRequest.java
+++ src/main/java/com/neuma573/autoboard/like/model/dto/LikeRequest.java
@@ -3,8 +3,10 @@ package com.neuma573.autoboard.like.model.dto;
 import com.neuma573.autoboard.like.model.entity.Like;
 import com.neuma573.autoboard.post.model.entity.Post;
 import com.neuma573.autoboard.user.model.entity.User;
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 public class LikeRequest {
     private Long postId;
