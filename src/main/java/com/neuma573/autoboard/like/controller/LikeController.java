57ca36eae4ee2b14cbff2fdf0b12e79a66416f7b
neuma573 <crye@kakao.com>
2024-05-17 11:37:47 +0900

    feat: 좋아요 토글 엔드 포인트 작성

--- src/main/java/com/neuma573/autoboard/like/controller/LikeController.java
+++ src/main/java/com/neuma573/autoboard/like/controller/LikeController.java
@@ -2,13 +2,13 @@ package com.neuma573.autoboard.like.controller;
 
 import com.neuma573.autoboard.global.model.dto.Response;
 import com.neuma573.autoboard.global.utils.ResponseUtils;
+import com.neuma573.autoboard.like.model.dto.LikeResponse;
 import com.neuma573.autoboard.like.service.LikeService;
+import com.neuma573.autoboard.security.utils.JwtProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +19,18 @@ public class LikeController {
 
     private final ResponseUtils responseUtils;
 
+    private final JwtProvider jwtProvider;
+
     @GetMapping("/{postId}")
     public ResponseEntity<Response<Long>> countLikes(@PathVariable Long postId) {
 //        int likeCount = likeService.countLikes(postId);
         return ResponseEntity.ok().body(responseUtils.success(0L));
+    }
+
+    @PostMapping("/{postId}")
+    public ResponseEntity<Response<LikeResponse>> toogleLike(@PathVariable Long postId, HttpServletRequest httpServletRequest) {
+        Long userId = jwtProvider.parseUserId(httpServletRequest);
+        return ResponseEntity.ok().body(responseUtils.success(likeService.toogleLike(postId, userId)));
     }
 
 }
