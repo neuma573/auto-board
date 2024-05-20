90242b5307bc86b48329efea2b6da0f1b4aacfbc
neuma573 <crye@kakao.com>
2024-05-20 10:30:02 +0900

    feat : LikeRepository에 좋아요 취소를 구현하기 위한 특정 사용자와 게시물에 대한 좋아요를 조회할 수 있는 findByUserAndPost 메서드 추가

--- src/main/java/com/neuma573/autoboard/like/repository/LikeRepository.java
+++ src/main/java/com/neuma573/autoboard/like/repository/LikeRepository.java
@@ -1,8 +1,14 @@
 package com.neuma573.autoboard.like.repository;
 
 import com.neuma573.autoboard.like.model.entity.Like;
+import com.neuma573.autoboard.post.model.entity.Post;
+import com.neuma573.autoboard.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Long countByPostId(Long id);
+
+    Optional<Like> findByUserAndPost(User user, Post post);
 }
