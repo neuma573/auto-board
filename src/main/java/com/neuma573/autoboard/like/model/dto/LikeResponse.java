303d33fee8853d5431a8cd92a3db1cca4d7530a8
neuma573 <crye@kakao.com>
2024-05-20 09:19:51 +0900

    feat : LikeResponse DTO에 메시지 필드 및 좋아요 제거 응답 메서드 추가

--- src/main/java/com/neuma573/autoboard/like/model/dto/LikeResponse.java
+++ src/main/java/com/neuma573/autoboard/like/model/dto/LikeResponse.java
@@ -17,11 +17,21 @@ public class LikeResponse {
 
     private LocalDateTime createdAt;
 
+    private String message;
+
     public static LikeResponse of(Like like) {
         return LikeResponse.builder()
                 .postId(like.getPost().getId())
                 .userResponse(UserResponse.of(like.getCreatedBy()))
                 .createdAt(like.getCreatedAt())
+                .build();
+    }
+
+    public static LikeResponse ofRemovedLike(Long postId, UserResponse userResponse) {
+        return LikeResponse.builder()
+                .postId(postId)
+                .userResponse(userResponse)
+                .message("Like removed")
                 .build();
     }
 
