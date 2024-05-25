71f278953112c802f4c1eab79a62448cf28013fd
neuma573 <crye@kakao.com>
2024-05-25 12:47:15 +0900

    feat: ck에디터의 정상적인 사용을 위한 Jsoup 화이트리스트에 태그 및 속성 등록

--- src/main/java/com/neuma573/autoboard/global/utils/ContentSanitizer.java
+++ src/main/java/com/neuma573/autoboard/global/utils/ContentSanitizer.java
@@ -9,15 +9,31 @@ public class ContentSanitizer {
     public static String filterHtmlSource(String source) {
         Safelist safelist = Safelist.basicWithImages();
 
-        safelist.addTags("figure");
-        safelist.addAttributes("figure", "class");
+        // CKEditor specific tags and attributes
+        safelist.addTags("h2", "h3", "h4", "br", "strong", "i", "figure", "div", "iframe", "table", "tbody", "tr", "td", "ul", "ol", "li", "span");
+        safelist.addAttributes("figure", "class", "contenteditable");
+        safelist.addAttributes("div", "class", "contenteditable", "role", "aria-label", "contenteditable", "dir", "lang");
+        safelist.addAttributes("iframe", "src", "width", "height", "frameborder", "allow", "allowfullscreen", "style");
+        safelist.addAttributes("td", "class", "role", "contenteditable");
+        safelist.addAttributes("span", "class", "data-cke-filler");
 
+        // Allow data-cke-filler attribute for <p> and <span> tags
+        safelist.addAttributes("p", "data-cke-filler");
+        safelist.addAttributes("span", "data-cke-filler");
+
+        // Allow specific attributes on img tags
         safelist.addAttributes("img", "style");
 
-        safelist.addTags("iframe");
-        safelist.addAttributes("iframe", "src", "width", "height", "frameborder", "allow", "allowfullscreen");
+        safelist.addAttributes("table", "class");
 
         safelist.addAttributes("blockquote", "cite");
+
+        safelist.addAttributes("div", "class", "title", "aria-hidden", "style");
+        safelist.addAttributes("svg", "xmlns", "viewBox", "width", "height", "fill", "version", "id", "xml:space", "xmlns:xlink");
+        safelist.addAttributes("path", "d");
+
+        safelist.addTags("a");
+        safelist.addAttributes("a", "href", "target", "rel");
 
         return Jsoup.clean(source, safelist);
     }
