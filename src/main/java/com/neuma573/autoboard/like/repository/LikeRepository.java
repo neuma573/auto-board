3a760d113945c9198f102e4686529d08e84137b0
neuma573 <crye@kakao.com>
2024-06-01 12:45:32 +0900

    feat: jpa 쿼리 메서드의 파라미터를 좀더 직관적인 이름으로 명명

--- src/main/java/com/neuma573/autoboard/like/repository/LikeRepository.java
+++ src/main/java/com/neuma573/autoboard/like/repository/LikeRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    Long countByPostId(Long id);
+    Long countByPostId(Long postId);
 
     Optional<Like> findByCreatedByAndPost(User user, Post post);
 }
