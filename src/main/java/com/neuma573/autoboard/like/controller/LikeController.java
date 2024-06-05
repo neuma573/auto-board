1a793ece27a6d03e140f33163b1386b22e997897
neuma573 <crye@kakao.com>
2024-06-05 10:52:05 +0900

    feat: 좋아요 출력 / 좋아요 토글 엔드포인트에 권한체크 적용

--- src/main/java/com/neuma573/autoboard/like/controller/LikeController.java
+++ src/main/java/com/neuma573/autoboard/like/controller/LikeController.java
@@ -4,6 +4,8 @@ import com.neuma573.autoboard.global.model.dto.Response;
 import com.neuma573.autoboard.global.utils.ResponseUtils;
 import com.neuma573.autoboard.like.model.dto.LikeResponse;
 import com.neuma573.autoboard.like.service.LikeService;
+import com.neuma573.autoboard.post.model.annotation.CheckPostAccess;
+import com.neuma573.autoboard.post.model.enums.PostAction;
 import com.neuma573.autoboard.security.utils.JwtProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +23,13 @@ public class LikeController {
 
     private final JwtProvider jwtProvider;
 
+    @CheckPostAccess(action = PostAction.READ)
     @GetMapping("/{postId}")
     public ResponseEntity<Response<Long>> countLikes(@PathVariable Long postId) {
-//        int likeCount = likeService.countLikes(postId);
         return ResponseEntity.ok().body(responseUtils.success(likeService.getLikeCount(postId)));
     }
 
+    @CheckPostAccess(action = PostAction.READ)
     @PostMapping("/{postId}")
     public ResponseEntity<Response<LikeResponse>> toogleLike(@PathVariable Long postId, HttpServletRequest httpServletRequest) {
         Long userId = jwtProvider.parseUserId(httpServletRequest);
