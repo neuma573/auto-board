a00a0b7b199a926f58e655199c09842884c10687
neuma573 <crye@kakao.com>
2024-06-10 21:44:36 +0900

    fix : 유저 Response에서 id제거

--- src/main/java/com/neuma573/autoboard/user/model/dto/UserResponse.java
+++ src/main/java/com/neuma573/autoboard/user/model/dto/UserResponse.java
@@ -7,13 +7,11 @@ import lombok.Getter;
 @Getter
 @Builder
 public class UserResponse {
-    private Long id;
     private String email;
     private String name;
 
     public static UserResponse of(User user) {
         return UserResponse.builder()
-                .id(user.getId())
                 .email(user.getEmail())
                 .name(user.getName())
                 .build();
