561a97561e6959548b63079339d78a1d68fa76e2
neuma573 <crye@kakao.com>
2024-07-02 17:13:50 +0900

    feat: redis 레포지토리를 사용하지 않으니 불필요한 어노테이션은 삭제

--- src/main/java/com/neuma573/autoboard/AutoBoardApplication.java
+++ src/main/java/com/neuma573/autoboard/AutoBoardApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
-@EnableRedisRepositories
 @EnableAsync
 @EnableScheduling
 @EnableFeignClients
