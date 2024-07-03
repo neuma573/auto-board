35d74cf988651d63977e946424df391573511bbe
neuma573 <crye@kakao.com>
2024-07-03 10:15:23 +0900

    feat: 유저의 정책 동의 DTO-Request 역할을 수행할 PolicyAgreementRequest 추가

---
+++ src/main/java/com/neuma573/autoboard/policy/model/dto/PolicyAgreementRequest.java
@@ -0,0 +1,12 @@
+package com.neuma573.autoboard.policy.model.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PolicyAgreementRequest {
+
+    private Long policyId;
+
+    private Long userId;
+
+}
