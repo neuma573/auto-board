51a0fb954f8e94f5e77bab183c2ed28291254775
neuma573 <crye@kakao.com>
2024-07-04 11:30:45 +0900

    feat: PolicyAgreement의 Jpa 레파지토리 추가

---
+++ src/main/java/com/neuma573/autoboard/policy/repository/PolicyAgreementRepository.java
@@ -0,0 +1,8 @@
+package com.neuma573.autoboard.policy.repository;
+
+import com.neuma573.autoboard.policy.model.entity.PolicyAgreement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PolicyAgreementRepository extends JpaRepository<PolicyAgreement, Long> {
+
+}
