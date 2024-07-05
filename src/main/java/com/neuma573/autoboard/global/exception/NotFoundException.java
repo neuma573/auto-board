684ac5d8053fcb6c2a50860cffa6dcef54def5b9
neuma573 <crye@kakao.com>
2024-07-05 12:45:12 +0900

    feat: 종합적으로 데이터가 없을 때 던질 예외 생성

---
+++ src/main/java/com/neuma573/autoboard/global/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.neuma573.autoboard.global.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
