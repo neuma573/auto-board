f35bdf00d918cf4659af197639faa1b84ac7b618
neuma573 <crye@kakao.com>
2024-07-07 16:41:30 +0900

    feat: PolicyAgreementRepository를 생성하고, 특정 정책에 대한 특정 유저의 정책 동의 사실을 리턴하는 쿼리 메서드 생성

--- src/main/java/com/neuma573/autoboard/policy/repository/PolicyAgreementRepository.java
+++ src/main/java/com/neuma573/autoboard/policy/repository/PolicyAgreementRepository.java
@@ -1,8 +1,13 @@
 package com.neuma573.autoboard.policy.repository;
 
+import com.neuma573.autoboard.policy.model.entity.Policy;
 import com.neuma573.autoboard.policy.model.entity.PolicyAgreement;
+import com.neuma573.autoboard.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface PolicyAgreementRepository extends JpaRepository<PolicyAgreement, Long> {
 
+    Optional<PolicyAgreement> findByAgreedByAndAgreedFor(User agreedBy, Policy AgreedFor);
 }
