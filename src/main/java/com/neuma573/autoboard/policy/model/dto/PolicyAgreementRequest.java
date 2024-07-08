b88b1aa269861da9cead8c204e74d515d3a837b2
neuma573 <crye@kakao.com>
2024-07-08 09:22:16 +0900

    feat: PolicyAgreementRequest에 엔티티 변환 메서드 생성

--- src/main/java/com/neuma573/autoboard/policy/model/dto/PolicyAgreementRequest.java
+++ src/main/java/com/neuma573/autoboard/policy/model/dto/PolicyAgreementRequest.java
@@ -1,12 +1,26 @@
 package com.neuma573.autoboard.policy.model.dto;
 
+import com.neuma573.autoboard.policy.model.entity.Policy;
+import com.neuma573.autoboard.policy.model.entity.PolicyAgreement;
+import com.neuma573.autoboard.user.model.entity.User;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class PolicyAgreementRequest {
 
     private Long policyId;
-
+    @Setter
     private Long userId;
 
+    public PolicyAgreement toEntity(User user, Policy policy) {
+        return PolicyAgreement.builder()
+                .agreedBy(user)
+                .isAgreed(true)
+                .agreedFor(policy)
+                .agreedAt(LocalDateTime.now())
+                .build();
+    }
 }
