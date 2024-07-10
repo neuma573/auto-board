16af2ce494f4470dd3b6cad57c5a6ae5fae9c6ac
neuma573 <crye@kakao.com>
2024-07-10 17:55:38 +0900

    feat: PolicyService와 UserService 간의 순환 참조를 피하기 위해 의존성 수정       - 정책 동의 시 동의 내용 로깅 추가       - 최신 정책 ID를 반환하는 메서드 구현       - 상수를 다른 서비스에서 접근할 수 있도록 public static으로 변경

--- src/main/java/com/neuma573/autoboard/policy/service/PolicyService.java
+++ src/main/java/com/neuma573/autoboard/policy/service/PolicyService.java
@@ -9,27 +9,25 @@ import com.neuma573.autoboard.policy.model.entity.Policy;
 import com.neuma573.autoboard.policy.model.entity.PolicyAgreement;
 import com.neuma573.autoboard.policy.repository.PolicyAgreementRepository;
 import com.neuma573.autoboard.policy.repository.PolicyRepository;
-import com.neuma573.autoboard.user.model.entity.User;
-import com.neuma573.autoboard.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PolicyService {
 
     private final PolicyRepository policyRepository;
 
-    private final UserService userService;
-
     private final PolicyAgreementRepository policyAgreementRepository;
 
-    private final String TERM_OF_USE = "이용약관";
+    public static final String TERM_OF_USE = "이용약관";
 
-    private final String PRIVACY_POLICY = "개인정보 처리방침";
+    public static final String PRIVACY_POLICY = "개인정보 처리방침";
 
-    private final String CONSENT_TO_COLLECTION_AND_USE_OF_PERSONAL_INFORMATION = "개인정보 수집 및 이용에 대한 동의";
+    public static final String CONSENT_AGREEMENT = "개인정보 수집 및 이용에 대한 동의";
 
     @Transactional(readOnly = true)
     public PolicyResponse getTermOfUse() {
@@ -43,7 +41,7 @@ public class PolicyService {
 
     @Transactional(readOnly = true)
     public PolicyResponse getConsentPolicy() {
-        return PolicyResponse.of(policyRepository.findFirstByPolicyNameOrderByCreatedAtDesc(CONSENT_TO_COLLECTION_AND_USE_OF_PERSONAL_INFORMATION).orElseThrow(AccessDeniedException::new));
+        return PolicyResponse.of(policyRepository.findFirstByPolicyNameOrderByCreatedAtDesc(CONSENT_AGREEMENT).orElseThrow(AccessDeniedException::new));
     }
 
     @Transactional(readOnly = true)
@@ -51,15 +49,21 @@ public class PolicyService {
         return policyRepository.findById(policyId).orElseThrow(() -> new NotFoundException("정책이 존재하지 않습니다."));
     }
 
+    @Transactional(readOnly = true)
+    public Long getLatestPolicyId(String policyName) {
+        return policyRepository.findFirstByPolicyNameOrderByCreatedAtDesc(policyName)
+                .map(Policy::getId)
+                .orElseThrow(() -> new NotFoundException("정책이 존재하지 않습니다."));
+    }
+
     @Transactional
     public PolicyAgreementResponse submitPolicyAgreement(PolicyAgreementRequest policyAgreementRequest) {
-        User user = userService.getUserById(policyAgreementRequest.getUserId());
         Policy policy = getPolicyById(policyAgreementRequest.getPolicyId());
-        policyAgreementRepository.findByAgreedByAndAgreedFor(user, policy).ifPresent(existingAgreement -> {
+        policyAgreementRepository.findByAgreedByAndAgreedFor(policyAgreementRequest.getUser(), policy).ifPresent(existingAgreement -> {
             throw new IllegalStateException("이미 동의된 정책입니다.");
         });
-        PolicyAgreement policyAgreement = policyAgreementRepository.save(policyAgreementRequest.toEntity(user, policy));
-
+        PolicyAgreement policyAgreement = policyAgreementRepository.save(policyAgreementRequest.toEntity(policy));
+        log.info("동의자: {}, 동의 정책: {}, 동의 일자: {}", policyAgreement.getAgreedBy().getId(), policyAgreement.getAgreedFor().getId(), policyAgreement.getAgreedAt());
         return PolicyAgreementResponse.of(policyAgreement);
     }
 }
