ac6a02d61832861dc7e5c2c4d062b0b1b4745897
neuma573 <crye@kakao.com>
2024-07-10 16:56:26 +0900

    feat: feat: UserService와의 순환 참조 문제 해결하기 위해, PolicyAgreementRequest의 유저Id필드는 유저 entity로 변경. 앞으로 컨트롤러에서 초기화할 예정

--- src/main/java/com/neuma573/autoboard/policy/model/dto/PolicyAgreementRequest.java
+++ src/main/java/com/neuma573/autoboard/policy/model/dto/PolicyAgreementRequest.java
@@ -3,19 +3,21 @@ package com.neuma573.autoboard.policy.model.dto;
 import com.neuma573.autoboard.policy.model.entity.Policy;
 import com.neuma573.autoboard.policy.model.entity.PolicyAgreement;
 import com.neuma573.autoboard.user.model.entity.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Getter
 public class PolicyAgreementRequest {
 
     private Long policyId;
     @Setter
-    private Long userId;
+    private User user;
 
-    public PolicyAgreement toEntity(User user, Policy policy) {
+    public PolicyAgreement toEntity(Policy policy) {
         return PolicyAgreement.builder()
                 .agreedBy(user)
                 .isAgreed(true)
